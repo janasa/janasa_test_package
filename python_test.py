563ebd7b8f7d018718e29afdec234a679f68f6de
Thompson <jana.a.thompson@accenture.com>
2016-11-16 13:08:19 -0800

    copying everything

---
+++ python_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+"""
+a test file for creating python packages
+"""
+
+if __name__ == "__main__":
+  for i in xrange(100):
+    print "yo"
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup = (
+  name = 'janasa_test_package',
+  packages = ['janasa_test_package'],
+  version = 0.0.1,
+  description = 'a test package to look at testpypi issues',
+  author = 'Jana Thompson',
+  author_email = 'luxzia@gmail.com',
+  url = #TODO - create repo and put url here,
+  download_url = #TODO - create repo and put url here,
+  keywords = ["testing"],
+  classifiers = [],
+)
